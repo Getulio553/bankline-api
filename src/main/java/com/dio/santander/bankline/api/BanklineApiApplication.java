20c18cdbf2e40934fef7dd0fa5d57130bacb8e95
Getulio553 <macedogetulio1@gmail.com>
2022-05-04 08:05:32 -0300

    alterando o arquivo principal

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,6 +8,7 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		//iremos começar a codificar o nosso projeto
 	}
 
 }
