279ddf792140ed27717d1aace3ed112217b76a45
Getulio553 <macedogetulio1@gmail.com>
2022-05-05 10:55:10 -0300

    Novas alterações

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -22,11 +22,17 @@ public class Movimentacao {
 	
 	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
+	
 	private String descricao;
+	
 	private Double valor;
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -56,6 +62,12 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 }
